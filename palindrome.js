fb00d52386f8cbe47be3adc874f1c8b80512219d
MIR SUHAIB <suhaibmir275025@gmail.com>
2022-12-14 22:56:00 +0530

    Palindrome Updated

--- palindrome.js
+++ palindrome.js
@@ -1,14 +1,18 @@
 function reverse(string){
-	let bag;
-	for(let i=string.length-1; i>=0; i--){
-		bag+=string[i];
-	}
-	return bag;
+
+let bag="";
+      for(let i=string.length-1;i>=0;i--)
+      { 
+       bag+=string[i];
+                       }
+       return bag;
 }
-let rev=reverse(string);
-if(string==rev){
-	console.log("Palindrome");
+   let rev=reverse(juhi);
+      if(string==rev)
+{
+      console.log("palindrome");
 }
-else
-	console.log("NA");
+       else
+{
+      console.log("NA");
 }
